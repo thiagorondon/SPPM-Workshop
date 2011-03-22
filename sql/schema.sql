ac3f6f21055cde8c788ab810188487431528f920
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2011-03-21 23:26:31 -0300

    Atualizando o schema.sql, e adicionando restrição ao cpf no Inscricao.pm
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,6 +1,7 @@
 create table inscricao (
        id integer primary key,
        nome text not null,
+       cpf text not null,
        apelido text,
        email text not null,
        celular integer not null,
